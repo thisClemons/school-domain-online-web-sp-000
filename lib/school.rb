d658ba2a604438b4c656bf8fcf6f41a229c3cc70
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-23 22:19:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,14 @@ class School
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
+
+  def add_student(student, grade)
+    @roster[grade] = [] if !@roster.include?(grade)
+    @roster[grade] << student
+
+  end
+
 
 end
